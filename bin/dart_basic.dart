e2cf5ef284543f227dbe74114c09160c2f15825f
Song's house <zken14@gmail.com>
2023-09-25 13:14:48 +0900

    추상화

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -1,7 +1,44 @@
-void main(List<String> arguments) {
-  const API_KEY = '123SAD123435ASDasdkas';
+abstract class Human {
+  void walk();
+}
 
-  // 데이터 타입
+enum Team { red, blue }
 
-  String name = '홍길동';
+class Player extends Human {
+  String name;
+  int xp;
+  Team team;
+
+  Player({
+    required this.name,
+    required this.xp,
+    required this.team,
+  });
+
+  @override
+  void walk() {
+    print("${this.name} is walking");
+  }
+
+  void sayHello() {
+    print("hello, i'm ${this.name}, my team color is ${this.team.name}");
+  }
+}
+
+class Coach extends Human {
+  @override
+  void walk() {
+    print('coach is walking!');
+  }
+}
+
+void main(List<String> args) {
+  var song = Player(
+    name: 'song',
+    xp: 1200,
+    team: Team.red,
+  );
+
+  song.sayHello();
+  song.walk();
 }
